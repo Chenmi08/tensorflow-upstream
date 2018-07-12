8e6e7d9eadb9491725606c025ffc47044165be0c
Wen-Heng (Jack) Chung <whchung@gmail.com>
2018-07-12 23:16:35 +0000

    [XLA:GPU] make GpuExecutable abstract and implement NVPTXExecutable
    To cope with upcoming introduction of AMDGPU support in XLA, make runtime
    compatibility check an abstract interface in GpuExecutable and move the logic
    to the concrete subclass NVPTXExecutable.

--- tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
+++ tensorflow/compiler/xla/service/gpu/gpu_compiler.cc
@@ -52,6 +52,7 @@ limitations under the License.
 #include "tensorflow/compiler/xla/service/gpu/ir_emitter_unnested.h"
 #include "tensorflow/compiler/xla/service/gpu/llvm_gpu_backend/gpu_backend_lib.h"
 #include "tensorflow/compiler/xla/service/gpu/multi_output_fusion.h"
+#include "tensorflow/compiler/xla/service/gpu/nvptx_executable.h"
 #include "tensorflow/compiler/xla/service/gpu/pad_insertion.h"
 #include "tensorflow/compiler/xla/service/gpu/partition_assignment.h"
 #include "tensorflow/compiler/xla/service/gpu/stream_assignment.h"
@@ -676,15 +677,15 @@ StatusOr<std::unique_ptr<Executable>> GpuCompiler::RunBackend(
         CreateHloProfilePrinterData(*profile_index_map, cost_analysis);
   }
 
-  auto* gpu_executable = new GpuExecutable(
+  auto* nvptx_executable = new NVPTXExecutable(
       ptx, cubin, {cc_major, cc_minor}, std::move(thunk_schedule),
       std::move(module), std::move(buffer_assignment),
       std::move(profile_printer), std::move(profile_index_map));
   if (embed_ir_in_executable) {
     DCHECK_NE("", ir_module_string_before_opt);
-    gpu_executable->set_ir_module_string(ir_module_string_before_opt);
+    nvptx_executable->set_ir_module_string(ir_module_string_before_opt);
   }
-  return std::unique_ptr<Executable>(gpu_executable);
+  return std::unique_ptr<Executable>(nvptx_executable);
 }
 
 std::vector<uint8> GpuCompiler::CompilePtxOrGetCachedResult(const string& ptx,
